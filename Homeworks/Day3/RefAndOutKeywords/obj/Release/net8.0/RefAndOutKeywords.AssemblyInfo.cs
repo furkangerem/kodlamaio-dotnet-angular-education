6a45d66dd6ef734bb83004a9ac674cf1d6a1859f
furkangerem <furkangerem@hotmail.com>
2024-08-31 00:33:31 +0300

    Homework | Day 4

--- Homeworks/Day3/RefAndOutKeywords/obj/Release/net8.0/RefAndOutKeywords.AssemblyInfo.cs
+++ Homeworks/Day3/RefAndOutKeywords/obj/Release/net8.0/RefAndOutKeywords.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RefAndOutKeywords")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98de94118953a047444245d7c92ee316be94a5c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3def26a17a3d6452d12dc4043d33a14e99bcb8f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RefAndOutKeywords")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RefAndOutKeywords")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Homeworks/Day4/Program.cs
@@ -0,0 +1,9 @@
+﻿using Day4;
+
+MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+myDictionary.Add(1, "Furkan");
+myDictionary.Add(2, "Kerem");
+myDictionary.Add(3, "Emirhan");
+// myDictionary.Add(3, "Emirhan"); Error case.
+
+myDictionary.ShowAll();
