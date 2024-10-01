2ac0c10951469270fa53766e7fb3dab9d0cc238e
furkangerem <furkangerem@hotmail.com>
2024-10-01 21:20:43 +0300

    Lecture | Day 11

---
+++ FinalProject/WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] // Attribute, In Java we call it Annotation
+    public class ProductsController : ControllerBase
+    {
+        // Loosely Coupled
+        // IoC Container - Inversion of Control
+        IProductService _iProductService;
+
+        public ProductsController(IProductService iProductService)
+        {
+            _iProductService = iProductService;
+        }
+
+        [HttpGet("getAll")]
+        public IActionResult GetAll()
+        {
+            var result = _iProductService.GetAll();
+            if (!result.IsSuccess)
+                return NotFound(result);
+
+            return Ok(result);
+        }
+
+        [HttpGet("getById")]
+        public IActionResult GetById(int productId)
+        {
+            var result = _iProductService.GetById(productId);
+            if (!result.IsSuccess)
+                return BadRequest(result);
+
+            return Ok(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _iProductService.Add(product);
+            if (!result.IsSuccess)
+                return BadRequest(result);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ FinalProject/WebAPI/Program.cs
@@ -0,0 +1,35 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// IoC Integration (In Java, it's Bean Configuration)
+// In Spring, it's Constructor Injection.
+builder.Services.AddSingleton<IProductService, ProductManager>();
+// We need to create a reference for IProductDal(). It's using in ProductManager;
+builder.Services.AddSingleton<IProductDal, EfProductDal>(); 
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
